ab32ce64a22aabd5846febd55391f9fbaaa7b145
Gil-Mor <gilmor11@gmail.com>
2023-08-11 22:52:35 +0200

    align-images.sh: mkdir

--- align-images.sh
+++ align-images.sh
@@ -5,4 +5,5 @@ set -ex
 find myimages/ -name "*.webp" -exec [ ! -f {}.jpg ] \; -exec  mv {} {}.jpg \;
 find myimages/ -name "*.jp*g" -exec [ ! -f {}.png ] \; -exec mogrify -format png {} \;
 
+mkdir -p myimages/aligned_images/
 python3 code/utils/align_images.py myimages/ myimages/aligned_images/ --output_size=1024
